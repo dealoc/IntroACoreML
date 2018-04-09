4b7850178cfbbff738df25ae2ec10934ed03e1ce
Dea-loC <dealoc@gmail.com>
2018-04-09 12:05:29 +0200

    Ajout fonctionnement bouton gallery

---
+++ IntroACoreML/librairieExtension.swift
@@ -0,0 +1,25 @@
+//
+//  librairieExtension.swift
+//  IntroACoreML
+//
+//  Created by Dea-loC on 09/04/2018.
+//  Copyright © 2018 Dea-loC. All rights reserved.
+//
+
+import UIKit
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            photoChoisieImageView.image = image
+            
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+}
